f9100a6d95307c7bb81601afbf69929a638a32ad
Will Forsberg <forsbergw82@protonmail.com>
2023-11-22 12:17:32 -0600

    Move build script here

---
+++ build.sh
@@ -0,0 +1,10 @@
+# Build script for Cloudflare Pages
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$PATH:$HOME/.cargo/bin"
+cargo install
+cargo install wasm-pack
+wasm-pack build --target no-modules
+mkdir www/pkg
+cp pkg/website.js www/pkg
+cp pkg/website_bg.wasm www/pkg
